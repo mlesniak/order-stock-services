428f1ad0fb629f80e04291832151f05f91d1fd66
Michael Lesniak <mail@mlesniak.com>
2023-07-26 04:50:10 +0200

    Add log output on startup

--- order-service/src/main/kotlin/com/mlesniak/order/OrderApplication.kt
+++ order-service/src/main/kotlin/com/mlesniak/order/OrderApplication.kt
@@ -1,5 +1,7 @@
 package com.mlesniak.order
 
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
@@ -8,7 +10,13 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @SpringBootApplication
 @EnableFeignClients
 @EnableMongoRepositories
-class OrderApplication
+class OrderApplication : CommandLineRunner {
+    private val log = KotlinLogging.logger {}
+
+    override fun run(vararg args: String?) {
+        log.info { "Starting order service" }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<OrderApplication>(*args)
